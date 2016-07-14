318e4014b5aaf283b82fcf84019ed2b3bc504725
chin8628 <boon.8628@hotmail.com>
2016-07-14 14:46:23 +0700

    Add urls

--- gcalendar_gen_class/genclass/urls.py
+++ gcalendar_gen_class/genclass/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url, include
 from genclass import views
 
 urlpatterns = [
-    url(r'^$', views.index, name="index")
+    url(r'^$', views.index, name="index"),
+    url(r'^success/', views.success, name="success"),
+    url(r'^help/', views.help, name="help"),
 ]
